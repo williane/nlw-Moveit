2a340da859966323cd40e0d8536b77684d1f55d1
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2021-02-23 19:54:50 -0300

    segunda aula nlw

---
+++ src/components/CompletedChallegens.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/CompletedChallegens.module.css'
+
+export function CompletedChallegens (){
+    return (
+        <div className={styles.completedChallegensContainer}>
+            <span>Desafios completos</span>
+            <span>5</span>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { CompletedChallegens } from "../components/CompletedChallegens";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
+
+      <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallegens />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
